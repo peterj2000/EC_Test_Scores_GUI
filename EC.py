877fd561263bae3b5426a209ef9db377e065e355
peter <pj@test.com>
2022-10-04 15:14:29 -0500

    update

---
+++ EC.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+root = Tk()
+
+width = 800
+height = 600
+
+def avg_score():
+
+    _test1 = float(test1.get())
+    _test2 = float(test2.get())
+    _test3 = float(test3.get())
+
+    average = (_test1+_test2+_test3)/3
+
+    average_score.config(text=f"{average}")
+
+
+
+frame = Frame(root)
+frame.pack()
+
+frame1 = Frame(root)
+frame1.pack()
+
+frame2 = Frame(root)
+frame2.pack()
+
+frame3 = Frame(root)
+frame3.pack()
+
+frame4 = Frame(root)
+frame4.pack()
+
+
+Label(frame, text="Enter score for test 1: ").pack(side=LEFT)
+test1 = Entry(frame,width=10)
+test1.pack(side=RIGHT)
+
+Label(frame1, text="Enter score for test 2: ").pack(side=LEFT)
+test2 = Entry(frame1,width=10)
+test2.pack(side=RIGHT)
+
+Label(frame2, text="Enter score for test 3: ").pack(side=LEFT)
+test3 = Entry(frame2,width=10)
+test3.pack(side=RIGHT)
+
+Label(frame3, text="Average ").pack(side=LEFT)
+
+average_score = Label(frame3)
+average_score.pack(side=RIGHT)
+
+Button(frame4, text="Average", width=15,
+       command=avg_score).pack(side=LEFT)
+
+
+Button(frame4, text="Exit", width=15,
+       command=root.destroy).pack(side=RIGHT)
+
+root.mainloop()
